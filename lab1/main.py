2325f63f341201b788a2cd98c81d449a0e9350ea
linkdew <luftaesthetics@gmail.com>
2021-09-15 21:52:53 +0300

    task init

---
+++ lab1/main.py
@@ -0,0 +1,35 @@
+def solution(s):
+    result = []
+
+    def allNaturalDivisors(number):
+        divarray = []
+        for k in range(2, int((number / 2)) + 1):
+            if number % k == 0:
+                divarray.append(k)
+        return divarray
+
+    divisors = allNaturalDivisors(len(s))
+
+    for i in range(len(divisors)):
+        result.append(s.count(s[:divisors[i]]))
+
+    if len(result) == 0 and s != len(s) * s[0]:
+        return 1
+    elif len(result) == 0 and s == len(s) * s[0]:
+        return len(s)
+    else:
+        return max(result)
+
+
+def test():
+    assert solution("abccbaabccba") == 2, "Check your implementation!"
+    assert solution("abcabcabcabc") == 4, "Check your implementation!"
+    assert solution("abcabcabcabcabcabcabcabc") == 8, "Check your implementation!"
+    assert solution("abccba" * 33) == 33, "Check your implementation!"
+    assert solution("gfgvghgdctnbc") == 1, "Check your implementation!"
+    assert solution("ababcav") == 1, "Check your implementation!"
+    print("Local tests for func passed!")
+
+
+if __name__ == "__main__":
+    test()
